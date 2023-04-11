74d9d903e830e2091bdd69b964159a925d2cba5f
NEKETA <molnei1996@gmail.com>
2023-04-11 10:24:12 +0300

    added zad all2

--- zad4.py
+++ zad4.py
@@ -6,7 +6,7 @@
 n = int(input("размер шоколадки n "))
 m = int(input("размер шоколадки m "))
 k = int(input("отломить k "))
-if k < n * m and ((k % n == 0) or (k % m == 0)):
+if k < n * m and k % n == 0 or k % m == 0:
     print('YES')
 else:
     print('NO')
